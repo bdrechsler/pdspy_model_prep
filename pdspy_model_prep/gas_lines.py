47cdbff55ddcba0f1627f03a3f4f211f97c39108
Blake Drechsler <wbd814@gmail.com>
2025-03-27 10:34:21 -0400

    added more precision to H2CO rest frequency

--- pdspy_model_prep/gas_lines.py
+++ pdspy_model_prep/gas_lines.py
@@ -3,5 +3,5 @@ line_dict = {'C18O': {'rest_freq': '219.56035410GHz', 'abundance': 1.8e-7, 'data
              'CH3OH': {'rest_freq': '241.80652400GHz', 'data_file': 'e-ch3oh.dat'},
              'C17O': {'rest_freq': '224.714743800GHz', 'abundance': 5.95e-8, 'data_file': 'c17o.dat'},
              '12CO': {'rest_freq': '230.538GHz', 'abundance': 1e-4, 'data_file': 'co.dat'},
-             'H2CO': {'rest_freq': '218.76GHz', 'abundance': 1e-9, 'data_file': 'ph2co-h2.dat'}}
+             'H2CO': {'rest_freq': '218.760066GHz', 'abundance': 1e-9, 'data_file': 'ph2co-h2.dat'}}
 
