e09865c1384995cfcc186d0ab1ead0e5c5dce261
Blake Drechsler <wbd814@gmail.com>
2025-03-27 10:38:16 -0400

    moved robust parameter to prep data method instead of model class

--- pdspy_model_prep/Model.py
+++ pdspy_model_prep/Model.py
@@ -7,7 +7,7 @@ from .create_batch_submit import create_batch_submit
 class Model:
 
     def __init__(self, source_name, dpc, vsys, line_name='C18O', chan_width='0.334km/s',
-                 nchan=42, robust=2, x0=[-0.5, 0.5], y0=[-0.5, 0.5], source_dir = './',
+                 nchan=42, x0=[-0.5, 0.5], y0=[-0.5, 0.5], source_dir = './',
                  disk_types=["truncated", "exptaper", "dartois-exptaper", "dartois-truncated"]):
 
         self.source_name = source_name
@@ -16,13 +16,12 @@ class Model:
         self.chan_width = chan_width
         self.nchan = nchan
         self.vsys = vsys
-        self.robust = robust
         self.x0=x0
         self.y0=y0
         self.source_dir = source_dir
         self.disk_types = disk_types
 
-    def prep_model(self, data=True, config=True, batch_script=True,
+    def prep_model(self, data=True, config=True, batch_script=True, robust=2.0,
                    remove_files=False, ncpu=1):
 
         # if there are ms files, prep the data
@@ -32,7 +31,7 @@ class Model:
             
             prep_data(source_name = self.source_name, source_dir = self.source_dir, 
                       line_name = self.line_name, chan_width = self.chan_width, 
-                      nchan = self.nchan, vsys = self.vsys, robust= self.robust, 
+                      nchan = self.nchan, vsys = self.vsys, robust= robust,
                       remove_files = remove_files)
 
         if config or batch_script:
